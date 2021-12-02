650ad586169a6a112336c270a015beb44ee42e07
charlesprd <73819652+chp567@users.noreply.github.com>
2021-12-02 12:22:09 +0100

    wpf simple

---
+++ WpfAppProjet/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using Microsoft.Win32;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppProjet
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //to open file source and dest
+        private void btnOpenFile_source(object sender, RoutedEventArgs e)
+        {
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.ValidateNames = false;
+            openFileDialog.CheckFileExists = false;
+            openFileDialog.CheckPathExists = true;
+            openFileDialog.FileName = "Folder Selection.";
+            if (openFileDialog.ShowDialog() == true)
+                txtEditor_source.Text = System.IO.Path.GetDirectoryName(openFileDialog.FileName);
+        }
+        private void btnOpenFile_dest(object sender, RoutedEventArgs e)
+        {
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.ValidateNames = false;
+            openFileDialog.CheckFileExists = false;
+            openFileDialog.CheckPathExists = true;
+            openFileDialog.FileName = "Folder Selection.";
+            if (openFileDialog.ShowDialog() == true)
+                txtEditor_dest.Text = System.IO.Path.GetDirectoryName(openFileDialog.FileName);
+        }
+
+
+
+
+    }
+}
