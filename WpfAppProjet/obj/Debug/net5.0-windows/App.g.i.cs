939f67373877b853d17909b956b624c9d8718050
baptiste-cassette <baptiste.cassette@viacesi.fr>
2021-12-14 16:50:44 +0100

    icon france

--- WpfAppProjet/obj/Debug/net5.0-windows/App.g.i.cs
+++ WpfAppProjet/obj/Debug/net5.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace WpfAppProjet {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.12.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace WpfAppProjet {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.12.0")]
         public static void Main() {
             WpfAppProjet.App app = new WpfAppProjet.App();
             app.InitializeComponent();
